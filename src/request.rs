5ec12195b24036c15fa2d08d6e63c93eb499b6c8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-05 20:18:52 -0300

    specify lifetimes

--- src/request.rs
+++ src/request.rs
@@ -1,18 +1,52 @@
 use std::convert::TryFrom;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::str;
 
-pub struct Request {
-    path: String,
-    arg: Option<String>,
+#[derive(Debug)]
+pub struct Request<'buf> {
+    path: &'buf str,
+    arg: Option<&'buf str>,
 }
 
-impl Request {
-    pub fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
+impl<'buf> Display for Request<'buf> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.display())
+    }
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> Request<'buf> {
+    pub fn display(&self) -> String {
+        match self.arg {
+            Some(arg) => format!("path: {}, arg: {}", self.path, arg),
+            None => format!("path: {}", self.path),
+        }
+    }
+}
+
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = String;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!()
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
+        let request = str::from_utf8(buf).map_err(|_| "Invalid request")?;
+
+        let (path, arg) = get_next_word(&request).ok_or("Invalid request")?;
+
+        match arg {
+            Some(arg) => Ok(Self {
+                path,
+                arg: Some(arg),
+            }),
+            None => Ok(Self { path, arg: None }),
+        }
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, Option<&str>)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], Some(&request[i + 1..])));
+        }
+    }
+
+    Some((&request[..], None))
 }
