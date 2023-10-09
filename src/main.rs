1cc7964e0b3c6b6f31e66f7de155b0b2d8841df4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-09 15:58:08 -0300

    make a small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::fs::File;
 
-mod utils;
 mod args;
 mod client;
 mod server;
+mod utils;
 
 use args::Args;
 use clap::Parser;
 use daemonize::Daemonize;
 
-use utils::{stderr, stdout};
 use client::Client;
 use server::tcp_handler::TCPHandler;
 use server::Server;
+use utils::{stderr, stdout};
 
 use crate::client::ClientError;
 
@@ -31,6 +31,10 @@ fn main() {
         };
     }
 
+    try_run_cmd(&client, &args);
+}
+
+fn try_run_cmd(client: &Client, args: &Args) {
     match client.clone().run("healthcheck;") {
         Ok(_) => {
             match client.clone().run("start;") {
@@ -40,7 +44,7 @@ fn main() {
         }
         Err(ClientError::ServerNotStarted) => {
             println!("Server not started, starting...");
-            start_daemon_server();
+            start_daemon_server()
         }
         Err(e) => stderr(format!("Error: {:?}", e).as_str()),
     };
@@ -64,6 +68,6 @@ fn start_daemon_server() {
         Err(e) => eprintln!("Error, {}", e),
     }
 
-    let server = Server::new(IP);
-    server.run(TCPHandler);
+    let new_server = Server::new(IP);
+    new_server.run(TCPHandler);
 }
