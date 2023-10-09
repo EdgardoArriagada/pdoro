d90dc923624f51b9a6a33f364afcec9fd793afa5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-09 13:00:52 -0300

    daemonize server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 mod app_io;
 mod args;
 mod client;
@@ -5,11 +7,14 @@ mod server;
 
 use args::Args;
 use clap::Parser;
+use daemonize::Daemonize;
 
 use app_io::{stderr, stdout};
 use client::Client;
 use server::tcp_handler::TCPHandler;
 use server::Server;
+
+use crate::client::ClientError;
 
 static IP: &'static str = "127.0.0.1:3030";
 
@@ -24,6 +29,36 @@ fn main() {
             Ok(v) => stdout(&v),
             Err(e) => stderr(format!("Error: {:?}", e).as_str()),
         };
+    }
+
+    let client = Client::new(IP);
+
+    match client.run("healthcheck;") {
+        Ok(v) => stdout(&v),
+        Err(ClientError::ServerNotStarted) => {
+            println!("Server not started, starting...");
+            start_daemon_server();
+        }
+        Err(e) => stderr(format!("Error: {:?}", e).as_str()),
+    };
+}
+
+fn start_daemon_server() {
+    let stdout_file = File::create("/tmp/pdoro.out").unwrap();
+    let stderr_file = File::create("/tmp/pdoro.err").unwrap();
+
+    let daemonize = Daemonize::new()
+        .working_directory("/tmp")
+        .user("nobody")
+        .group("pdoro_daemon")
+        .umask(0o777)
+        .stdout(stdout_file)
+        .stderr(stderr_file)
+        .privileged_action(|| "Executed before drop privileges");
+
+    match daemonize.start() {
+        Ok(_) => println!("Success, daemonized"),
+        Err(e) => eprintln!("Error, {}", e),
     }
 
     let server = Server::new(IP);
